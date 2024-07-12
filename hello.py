68d5b77ab62ef2d633a157f10d223f5e6028f440
aelhaj2 <152576545+aelhaj2@users.noreply.github.com>
2024-07-11 20:15:25 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ print("Hello, World!")
 print("Hello, World form Huda!")
 print("Hello, World!") 
 print("Hello, World form Brody!")
-
+print("Hello, World!")
+print("Hello, World from Abdeel!")
 
 print("This is brody's changes for group exercise part 4")
